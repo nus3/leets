0360951ec15dc2f86d303f241db5bff6241c3363
yota-hada <hadayota33@gmail.com>
2020-12-20 15:21:11 +0900

    solve TwoSum of anothre pattern.

--- two_sum/main.go
+++ two_sum/main.go
@@ -25,9 +25,27 @@ func twoSum(nums []int, target int) []int {
 	return []int{firstIndex, secondIndex}
 }
 
+func twoSum2(nums []int, target int) []int {
+	m := make(map[int]int)
+
+	for i, num := range nums {
+		idx, ok := m[target-num]
+		if ok {
+			return []int{idx, i}
+		}
+
+		// NOTE: mapのindexに値を埋め込み、target-numした時に合致したindexがあればそん時のvalueとindexを返す
+		m[num] = i
+	}
+
+	return nil
+}
+
 func main() {
-	nums := []int{3, 2, 4}
-	target := 6
-	output := twoSum(nums, target)
+	// nums := []int{3, 2, 4}
+	nums := []int{11, 15, 2, 7}
+	// target := 6
+	target := 9
+	output := twoSum2(nums, target)
 	fmt.Println(output)
 }
